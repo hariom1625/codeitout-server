0591e6aa4633919ceea12686bc88ed4ae8f5155b
Hari Om Yadav <hariomyadav.btech.cs19@iiitranchi.ac.in>
2021-03-05 17:16:25 +0530

    minor update

--- app.js
+++ app.js
@@ -17,11 +17,8 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
 res.send("Hello World eee!!")
 });
-let port = process.env.PORT;
-if(port== null || port == ""){
-port = 4000;
-}
-app.listen(port, () => console.log("Server started on Port: 4000"));
+
+app.listen(process.env.PORT||4000, () => console.log("Server started on Port: 4000"));
 
 // app.get("/",function(req,res){
 // // res.send({message :"Hello World !!"})
